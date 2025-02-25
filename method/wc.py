7a6f81d114af5d29c2219674562b44ea8f28cae4
gizmore <gizmore@wechall.net>
2025-02-25 06:40:09 +0100

    'sync'

---
+++ method/wc.py
@@ -0,0 +1,25 @@
+import aiohttp
+
+from gdo.base.GDT import GDT
+from gdo.base.Method import Method
+from gdo.core.GDT_String import GDT_String
+
+
+class wc(Method):
+
+    def gdo_parameters(self) -> [GDT]:
+        return [
+            GDT_String('username'),
+        ]
+
+    async def gdo_execute(self) -> GDT:
+        if username := self.param_val('username'):
+            return await self.for_user(username)
+        return await self.for_user(self._env_user.get_name())
+
+    async def for_user(self, username: str) -> GDT:
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://python.org') as response:
+                pass
+
+        pass
