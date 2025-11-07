4fe48a378701976ad388269183c81447d36b5c02
gizmore <gizmore@wechall.net>
2025-11-07 18:46:44 +0100

    'sd'

---
+++ method/defcon.py
@@ -0,0 +1,9 @@
+from gdo.form.GDT_Form import GDT_Form
+from gdo.form.MethodForm import MethodForm
+
+
+class defcon(MethodForm):
+
+    def gdo_create_form(self, form: GDT_Form) -> None:
+
+        super().gdo_create_form(form)
