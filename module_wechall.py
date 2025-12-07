b4d949de3b96c939a087db14c467afa18807abf1
gizmore <gizmore@wechall.net>
2025-12-07 09:59:22 +0100

    'forum'

--- module_wechall.py
+++ module_wechall.py
@@ -1,5 +1,7 @@
 from gdo.base.GDO_Module import GDO_Module
 from gdo.wechall.WC_Site import WC_Site
+from gdo.wechall.WC_SiteDescription import WC_SiteDescription
+from gdo.wechall.WC_SiteTags import WC_SiteTags
 from gdo.wechall.WC_SiteVoteDiff import WC_SiteVoteDiff
 from gdo.wechall.WC_SiteVoteFun import WC_SiteVoteFun
 
@@ -9,6 +11,8 @@ class module_wechall(GDO_Module):
     def gdo_classes(self):
         return [
             WC_Site,
+            WC_SiteDescription,
+            WC_SiteTags,
             WC_SiteVoteDiff,
             WC_SiteVoteFun,
         ]
